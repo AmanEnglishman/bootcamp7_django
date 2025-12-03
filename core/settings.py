8548aa77598b42dea69a086527c205f425d94f2a
Aman <aman13032006@gmail.com>
2025-12-03 18:59:09 +0600

    commit

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import MEDIA_URL
+from django.conf.global_settings import MEDIA_URL, EMAIL_BACKEND
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +132,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 JAZZMIN_UI_TWEAKS = {
     "theme": "darkly",
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'englishmanaman@gmail.com'
+EMAIL_HOST_PASSWORD = 'mzkivhadmeqsgsge'
--- news/views.py
+++ news/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.db.models import Q
+from django.core.mail import send_mail
+from django.conf import settings
 
 from .models import News
+from .forms import ContactForm
 
 def home(request):
     query = request.GET.get('q')
@@ -16,13 +19,40 @@ def home(request):
 
     return render(request, 'index.html', {'news': news, 'query': query})
 
-'''
-http://127.0.0.1:8000/?q=
-'''
+
 
 def contacts(request):
-    return render(request, 'contacts.html')
+    sent = False
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+
+            full_message = f"""
+                    Имя: {name}
+                    Email: {email}
+                    Сообщение:
+                    {message}
+                    """
+            send_mail(
+                subject="Новое сообщение с сайта",
+                message=full_message,
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=["your_email@gmail.com"],
+                fail_silently=False,
+            )
+
+            sent = True
+    else:
+        form = ContactForm()
+
+    return render(request, "contacts.html", {"form": form, "sent": sent})
+
 
 def news_detail(request, pk):
     news = News.objects.get(pk=pk)
     return render(request, 'news_detail.html', context={'news': news})
+
